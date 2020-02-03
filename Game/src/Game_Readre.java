81ce13e6901bbe80f642e73f000697856e1df806
armanayvazyan <arman.ayvazyan@picsart.com>
2020-02-04 00:32:07 +0400

    Changed Game logic, added grid layout

--- Game/src/Game_Readre.java
+++ Game/src/Game_Readre.java
@@ -1,135 +1,85 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Game_Readre extends JFrame {
-		public static void main(String[] args) { 
-			 	
+	public static void main(String[] args) {
 
-			JFrame jFrame = new JFrame("Game");
-	        jFrame.setLayout(null);
-	        jFrame.setBounds(0,0,1000,500);
-	        JPanel jPanel1 = new JPanel();
-	        JPanel jPanel2 = new JPanel();
-	        JPanel jPanel3 = new JPanel();
-	        JPanel jPanel4 = new JPanel();
-	        final ArrayList<ImageIcon> game = new ArrayList<ImageIcon>();
-	        
-	        jPanel1.setBounds(50,50,190,280);
-	        jPanel2.setBounds(250,50,190,280);
-	        jPanel3.setBounds(450,50,190,280);
-	        jPanel4.setBounds(650,50,190,280);
+		Random rand = new Random();
+		int rows = 2;
+		int cols = 2;
 
-	        final ImageIcon queen = new ImageIcon("C:/Users/Vach/Desktop/queen_of_diamonds2.png");
-	        final ImageIcon card = new ImageIcon("C:/Users/Vach/Desktop/images.png");
-	        final ImageIcon back = new ImageIcon("C:/Users/Vach/Desktop/45.PNG");
-	        
-	        final JLabel image1 = new JLabel(queen);
-	        image1.setBounds(642, 230, 100, 100);
-	        image1.setVisible(false);
-	        
-	        
-	        
-	        
-	        final JLabel image2 = new JLabel(back);
-	        image2.setBounds(642, 230, 100, 100);
-	        image2.setVisible(true);
-	        
-	        image2.addMouseListener(new MouseAdapter() {
-		        
-	     	   
-	        	@Override
-                public void mousePressed(MouseEvent e) {	
-	        		image2.setVisible(false);
-	        		image1.setVisible(true);
-	        		game.add(image1);
-	        	}
-	        	});
-	        
-	        final JLabel image3 = new JLabel(queen);
-	        image3.setBounds(642, 230, 100, 100);
-	        image3.setVisible(false);
-	        if(image3.getIcon().equals(image1.getIcon())) {
-	        	
-	        }
-	        
-	        final JLabel image4 = new JLabel(back);
-	        image4.setBounds(642, 230, 100, 100);
-	        image4.setVisible(true);
-	        
-	        image4.addMouseListener(new MouseAdapter() {
-		        
-		     	   
-	        	@Override
-                public void mousePressed(MouseEvent e) {	
-	        		image4.setVisible(false);
-	        		image3.setVisible(true);
-	        	}
-	        	});
-	        
-	        final JLabel image5 = new JLabel(card);
-	        image5.setBounds(642, 230, 100, 100);
-	        image5.setVisible(false);
-	        
-	        final JLabel image6 = new JLabel(back);
-	        image6.setBounds(642, 230, 100, 100);
-	        image6.setVisible(true);
-	        
-	        image6.addMouseListener(new MouseAdapter() {
-		        
-		     	   
-	        	@Override
-                public void mousePressed(MouseEvent e) {	
-	        		image6.setVisible(false);
-	        		image5.setVisible(true);
-	        	}
-	        	});
-	        
-	        
-	        final JLabel image7 = new JLabel(card);
-	        image7.setBounds(642, 230, 100, 100);
-	        image7.setVisible(false);
-	        
-	        final JLabel image8 = new JLabel(back);
-	        image8.setBounds(642, 230, 100, 100);
-	        image8.setVisible(true);
-	        	       
-	        image8.addMouseListener(new MouseAdapter() {
-		        
-		     	   
-	        	@Override
-                public void mousePressed(MouseEvent e) {	
-	        		image8.setVisible(false);
-	        		image7.setVisible(true);
-	        	}
-	        	});
-	        
+		Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
 
-	        jPanel1.add(image1);
-	        jPanel1.add(image2);
-	        jPanel2.add(image3);
-	        jPanel2.add(image4);
-	        jPanel3.add(image5);
-	        jPanel3.add(image6);
-	        jPanel4.add(image7);
-	        jPanel4.add(image8);
-	        
-	        
-	        jFrame.add(jPanel1);
-	        jFrame.add(jPanel2);
-	        jFrame.add(jPanel3);
-	        jFrame.add(jPanel4);
+		JFrame jFrame = new JFrame("Game");
+		jFrame.setLayout(null);
+		jFrame.setBounds(0,0,700,800);
+		JPanel panel = new JPanel();
+		panel.setBounds(30,30,500,500);
+		panel.setLayout(new GridLayout(rows,cols,0,0));
 
-	        
-	        
-	        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    	jFrame.setVisible(true);
-	        
-	      
+
+		final ArrayList<ImageIcon> gameCardCollector = new ArrayList<>();
+
+
+
+		final ImageIcon queen = new ImageIcon("Game/Images/queen.png");
+		final ImageIcon valet = new ImageIcon("Game/Images/valet.png");
+		final ImageIcon seven = new ImageIcon("Game/Images/seven.png");
+		final ImageIcon six = new ImageIcon("Game/Images/six.png");
+		final ImageIcon two = new ImageIcon("Game/Images/two.png");
+		final ImageIcon backCard = new ImageIcon("Game/Images/back.png");
+
+
+		for (int i = 0; i < rows; i++) {
+			gameCardCollector.add(queen);
 		}
-		 
+		for (int i = 0; i < cols; i++) {
+			gameCardCollector.add(valet);
+		}
+
+
+
+		for (int i = 0; i < rows ; i++) {
+			for (int j = 0; j < cols; j++) {
+
+			}
+		}
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				JLabel label = new JLabel();
+				label.setIcon(new ImageIcon(backCard.getImage().getScaledInstance(70, 120, Image.SCALE_SMOOTH)));
+				panel.add(label);
+
+				label.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						super.mouseClicked(e);
+						int num = rand.nextInt(gameCardCollector.size());
+						label.setIcon(new ImageIcon(gameCardCollector.get(num).getImage().getScaledInstance(70, 120, Image.SCALE_SMOOTH)));
+						gameCardCollector.remove(num);
+					}
+				});
+			}
+		}
+
+
+		jFrame.add(panel);
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jFrame.setVisible(true);
+
+
+	}
+
 }
+
+
 
 
